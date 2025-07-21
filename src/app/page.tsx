cbaa1bd1009439faaf54c2bce2c57704c943de5e
Laurin Schwenter <gschwenter.laurin@aol.com>
2025-07-21 18:24:49 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx or index.js
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
+      <a
+        href="https://www.linkedin.com/in/laurin-gschwenter/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-10 py-10 text-white bg-blue-600 hover:bg-blue-700 rounded-full text-lg font-semibold transition-colors"
+      ></a>
+    </div>
+  );
+}
